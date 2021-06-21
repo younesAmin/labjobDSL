f3194503b71bdf42e391042fc1ed9fd4e7fd6bda
Younes Amine <47544240+younesAmin@users.noreply.github.com>
2021-06-21 19:53:05 +0100

    Update job1.groovy

--- job1.groovy
+++ job1.groovy
@@ -1,6 +1,6 @@
 job('exampledsl1') {
   scm {
-    git('git@github.com:younesAmin/labjobDSL.git'){ node ->
+    git('https://github.com/younesAmin/labjobDSL.git'){ node ->
       node / gitConfigName('jobDSL')
       node / gitCinfigEmail('uhyounes@gmail.com')
        
